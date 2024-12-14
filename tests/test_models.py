57de15702e6ed808089c118df3996e519af1ef75
bettyje <bettyjelagat156@gmail.com>
2024-12-14 17:26:49 +0300

    Articles - With Database

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,18 +3,26 @@ from models.author import Author
 from models.article import Article
 from models.magazine import Magazine
 
+
 class TestModels(unittest.TestCase):
     def test_author_creation(self):
-        author = Author(1, "John Doe")
-        self.assertEqual(author.name, "John Doe")
+        author = Author(1, "Betty")
+        self.assertEqual(author.name, "Betty")
 
     def test_article_creation(self):
-        article = Article(1, "Test Title", "Test Content", 1, 1)
-        self.assertEqual(article.title, "Test Title")
+        author = Author(1, "Betty")
+        magazine = Magazine(1, "Vogue", "Technology")
+
+        article = Article(title="Vogue", author=author, magazine=magazine)
+
+        self.assertEqual(article.title, "Vogue")
+        self.assertEqual(article.author.name, "Betty")
+        self.assertEqual(article.magazine.name, "Vogue")
 
     def test_magazine_creation(self):
-        magazine = Magazine(1, "Tech Weekly")
-        self.assertEqual(magazine.name, "Tech Weekly")
+        magazine = Magazine(1, "Vogue", "Technology")
+        self.assertEqual(magazine.name, "Vogue")
+
 
 if __name__ == "__main__":
     unittest.main()
