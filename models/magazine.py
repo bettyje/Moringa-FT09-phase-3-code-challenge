e4a898bb5f70ec2a9104a91183a7638b631b57f1
bettyjay <bettyjelagat156@gmail.com>
2024-12-17 01:52:48 +0300

    Update magazine.py

--- models/magazine.py
+++ models/magazine.py
@@ -4,18 +4,35 @@ from database.connection import get_db_connection
 
 class Magazine:
     def __init__(self, id=None, name=None, category=None):
-        self._id = id
+        if id is None and name and category:
+            self._create_magazine_in_db(name, category)
+        else:
+            self._id = id
+            self._name = name
+            self._category = category
+
+    def _create_magazine_in_db(self, name, category):
+        """
+        Creates a new magazine in the database and sets its ID.
+        """
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise ValueError(
+                "Name must be a string between 2 and 16 characters.")
+        if not isinstance(category, str) or len(category) <= 0:
+            raise ValueError("Category must be a non-empty string.")
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO magazines (name, category) VALUES (?, ?)", (name, category))
+        conn.commit()
+        self._id = cursor.lastrowid
+        conn.close()
         self._name = name
         self._category = category
 
-        if id is None and name is not None and category is not None:
-            conn = get_db_connection()
-            cursor = conn.cursor()
-            cursor.execute(
-                "INSERT INTO magazines (name, category) VALUES (?, ?)", (name, category))
-            conn.commit()
-            self._id = cursor.lastrowid
-            conn.close()
+    def __str__(self):
+        return f"Magazine(id={self.id}, name='{self.name}', category='{self.category}')"
 
     @property
     def id(self):
@@ -27,11 +44,14 @@ class Magazine:
 
     @name.setter
     def name(self, value):
+        if not isinstance(value, str) or not (2 <= len(value) <= 16):
+            raise ValueError(
+                "Name must be a string between 2 and 16 characters.")
         self._name = value
         conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute(
-            'UPDATE magazines SET name = ? WHERE id = ?', (self._name, self.id))
+            "UPDATE magazines SET name = ? WHERE id = ?", (self._name, self.id))
         conn.commit()
         conn.close()
 
@@ -41,59 +61,12 @@ class Magazine:
 
     @category.setter
     def category(self, value):
+        if not isinstance(value, str) or len(value) <= 0:
+            raise ValueError("Category must be a non-empty string.")
         self._category = value
         conn = get_db_connection()
         cursor = conn.cursor()
         cursor.execute(
-            'UPDATE magazines SET category = ? WHERE id = ?', (self._category, self.id))
+            "UPDATE magazines SET category = ? WHERE id = ?", (self._category, self.id))
         conn.commit()
         conn.close()
-
-    def articles(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('''
-            SELECT * FROM articles WHERE magazine_id = ?
-        ''', (self.id,))
-        articles = cursor.fetchall()
-        conn.close()
-        from models.article import Article
-        return [Article(article["id"], article["title"], article["author_id"], article["magazine_id"]) for article in articles]
-
-    def contributors(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('''
-            SELECT authors.* FROM authors
-            JOIN articles ON authors.id = articles.author_id
-            WHERE articles.magazine_id = ?
-        ''', (self.id,))
-        authors = cursor.fetchall()
-        conn.close()
-        from models.author import Author
-        return [Author(author["id"], author["name"]) for author in authors]
-
-    def article_titles(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('''
-            SELECT title FROM articles WHERE magazine_id = ?
-        ''', (self.id,))
-        articles = cursor.fetchall()
-        conn.close()
-        return [article["title"] for article in articles] if articles else None
-
-    def contributing_authors(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('''
-            SELECT authors.* FROM authors
-            JOIN articles ON authors.id = articles.author_id
-            WHERE articles.magazine_id = ?
-            GROUP BY authors.id
-            HAVING COUNT(articles.id) > 2
-        ''', (self.id,))
-        authors = cursor.fetchall()
-        conn.close()
-        from models.author import Author
-        return [Author(author["id"], author["name"]) for author in authors] if authors else None
